64342de6ef6e9af7fb6b6216d947da16e0f3aec7
Gonzalo López <gonzalo.lopez.duran@estudiantat.upc.edu>
2026-03-24 12:07:56 +0100

    #68 Selecció-de-Ruta-i-càrrega-d'aquesta-en-la-UI Soluciona errors de carregar rutes

--- lib/shared/models/RouteModel.dart
+++ lib/shared/models/RouteModel.dart
@@ -13,6 +13,8 @@ class RouteModel {
   final LatLng startPoint;
   final LatLng endPoint;
   final String location;
+  final String altitude;
+  final String elevation_gain;
 
   RouteModel({
     required this.id,
@@ -25,22 +27,43 @@ class RouteModel {
     required this.startPoint,
     required this.endPoint,
     required this.location,
+    required this.altitude,
+    required this.elevation_gain,
   });
 
   factory RouteModel.fromJson(Map<String, dynamic> json) {
     return RouteModel(
-      id: json['id'],
+      id: json['route_id'].toString(),
       name: json['name'],
       distance: json['distance'].toDouble(),
       isPrivate: json['private'],
       creatorName: json['created_by'],
-      createdAt: json['created_at'],
-      trajectory: (json['trajectory'] as List)
-          .map((point) => LatLng(point['latitude'], point['longitude']))
-          .toList(),
-      startPoint: LatLng(json['start_point']['latitude'], json['startPoint']['longitude']),
-      endPoint: LatLng(json['end_point']['latitude'], json['endPoint']['longitude']),
+      createdAt: DateTime.parse(json['created_at']),
+      trajectory: json['trajectory'] != null
+          ? (json['trajectory'] as List).map((coord) {
+        return LatLng(
+            (coord[1] as num).toDouble(), // Latitud
+            (coord[0] as num).toDouble()  // Longitud
+        );
+      }).toList()
+          : [],
+
+      startPoint: json['start_point'] != null
+          ? LatLng(
+          (json['start_point'][1] as num).toDouble(), // Latitud
+          (json['start_point'][0] as num).toDouble()  // Longitud
+      )
+          : const LatLng(0, 0), // O null si tu variable no es requerida
+
+      endPoint: json['end_point'] != null
+          ? LatLng(
+          (json['end_point'][1] as num).toDouble(), // Latitud
+          (json['end_point'][0] as num).toDouble()  // Longitud
+      )
+          : const LatLng(0, 0), // O pon un LatLng(0,0) como arriba si lo prefieres
       location: json['location'],
+      altitude: json['altitude'].toString(),
+      elevation_gain: json['elevation_gain'].toString(),
     );
   }
 
@@ -56,6 +79,8 @@ class RouteModel {
       'start_point': {'latitude': startPoint.latitude, 'longitude': startPoint.longitude},
       'end_point': {'latitude': endPoint.latitude, 'longitude': endPoint.longitude},
       'location': location,
+      'altitude': altitude,
+      'elevation_gain': elevation_gain,
     };
   }
 }
