747e5642ae75814ea551bbf80222ed0a0f464c82
Gonzalo López <gonzalo.lopez.duran@estudiantat.upc.edu>
2026-04-23 10:13:32 +0200

    Afegeix servei d'activity

---
+++ lib/core/services/activity_service.dart
@@ -0,0 +1,37 @@
+import 'package:healthy_way_frontend/shared/models/RouteModel.dart';
+import 'package:healthy_way_frontend/shared/models/Activity.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ActivityService {
+
+  static final ActivityService _instance = ActivityService._internal();
+  factory ActivityService() => _instance;
+  ActivityService._internal();
+
+  final String baseUrl = 'http://localhost:3000/api/v1';
+
+  Future<dynamic> createActivity(Activity activityData) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/users/registeractivity'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(activityData.toJson()),
+    );
+
+    if (response.statusCode == 201) {
+      return json.decode(response.body);
+    } else {
+      print('--- DEBUG INFO ---');
+      print('URL: ${response.request?.url}'); // Verifica la URL final (por si hubo redirecciones)
+      print('METODO: ${response.request?.method}'); // Confirma que fue un POST
+      print('STATUS CODE: ${response.statusCode}'); // El número clave (404, 405, 500...)
+      print('BODY DEL ERROR: ${response.body}'); // Aquí el backend suele decir qué campo falla
+      print('HEADERS RESPUESTA: ${response.headers}'); // Ver qué tipo de contenido dice el servidor que envía
+      print('HEADERS ENVIADOS: ${response.request?.headers}'); // ¿Mandaste el Content-Type: application/json?
+      print('JSON ENVIADO: ${jsonEncode(activityData)}'); // Revisa qué estás mandando
+      print('------------------');
+      throw Exception('Error al crear la actividad');
+    }
+  }
+
+}
